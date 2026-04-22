4ad821d2bcac2c768edb0008c0a004cab4889476
Victor Brito <victor.bnemmanuel@gmail.com>
2026-04-22 15:18:00 -0300

    Iniciando interface

--- src/Main.java
+++ src/Main.java
@@ -21,7 +21,7 @@ public class Main {
             }
         }
     }
-        public static void calcularTributos(ArrayList < Conta > listaDeContas) {
+        public static void calcularTributos(ArrayList <Conta> listaDeContas) {
             double total = 0;
 
             for (Conta conta : listaDeContas) {
@@ -38,5 +38,28 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        ArrayList<Conta> contas = new ArrayList<>();
+
+        int resposta;
+
+        do {
+            System.out.println("-=--=-=-= Interface -=-=-=-=-=");
+            System.out.println("1 - Criar Conta");
+            System.out.println("2 - Listar Contas");
+            System.out.println("3 - Sacar");
+            System.out.println("4 - Depositar");
+            System.out.println("5 - Transferir");
+            System.out.println("6 - Calcular Tributos");
+            System.out.println("7 - Finalizar chamado");
+            System.out.println("Digite o número correspondente: ");
+
+            resposta = scanner.nextInt();
+
+            switch(resposta) {
+                
+            }
+
+        } while (resposta != 7);
     }
 }
